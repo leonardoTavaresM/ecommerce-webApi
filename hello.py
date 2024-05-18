45459e2bef8a597317dec34d58bb50422d073a17
leonardoTavaesM <leomaltadesa0712@gmail.com>
2024-05-18 17:38:52 -0300

    feat: adding products route

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,3 +6,21 @@ app = Flask(__name__)
 def hello_world():
   return '<p>Hello World!</p>'
 
+@app.route('/products')
+def products():
+  response = jsonify([
+    {
+      "title": "Caneca Personalizada de Porcelana do backends",
+      "amount": 123.45,
+      "installments": { "number": 3, "total": 41.15, "hasFee": True },
+    },
+    {
+      "title": "Caneca de Tulipa do backends",
+      "amount": 123.45,
+      "installments": { "number": 3, "total": 41.15, "hasFee": False },
+    },
+  ])
+
+  response.headers.add('Access-Control-Allow-Origin', '*')
+
+  return response
